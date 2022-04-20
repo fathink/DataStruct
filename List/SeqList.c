1a60f7bd0372511d40f0c7ccd5b65d9715618671
fazuo <ffzbest2008@gmail.com>
2022-04-20 21:32:40 +0800

    new file:   .gitignore 	new file:   List/SeqList.c 	new file:   test/hello 	new file:   test/hello.c

---
+++ List/SeqList.c
@@ -0,0 +1,22 @@
+#define OK 1;
+#define ERROR 0;
+
+typedef int Status;
+
+
+#define MAXSIZE 20
+typedef int ElemType;
+typedef struct
+{
+    ElemType data[MAXSIZE];
+    int length;
+}SqList;
+
+/* 获取顺序列表元素*/
+Status GetElem(SqList L, int i, ElemType *e)
+{
+    if(L.length==0 || i<0 || i>L.length)
+        return ERROR
+    *e=L.data[i];
+    return OK
+}
