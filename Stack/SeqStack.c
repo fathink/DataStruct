318f4e4de8fc3f0f23887e9b2fdadd542b4a0d60
fathink <ffzbest2008@gmail.com>
2022-07-08 00:09:55 +0800

    modified:   Stack/SeqStack.c

--- Stack/SeqStack.c
+++ Stack/SeqStack.c
@@ -26,18 +26,89 @@ typedef struct
 Status InitStatck(SeqStack *s)
 {
     s->top = -1;
+    return OK;
 }
 
-//pop,入栈
-Status pop()
+//入栈
+Status push(SeqStack *s, ElemType e)
+{
+    if(s->top==MAXSIZE){
+        return ERROR; //栈满返回ERROR
+    }
+    s->top++;
+    s->data[s->top] = e;
+    return OK;
+}
+
+
+//pop,出栈
+Status pop(SeqStack *s, ElemType *e)
+{
+    if(s->top==-1)
+    {
+        return ERROR; //栈为空，返回ERROR
+    }
+    
+    *e = s->data[s->top];  //返回栈顶元素
+    s->top--;
+
+    return OK;
+
+}
+
+void PrintStack(SeqStack *s)
+{
+    if(s->top == -1)
+    {
+        printf("Stack(%p) is NULL",s);
+    }
+    else
+    {
+        printf("Bottom");
+        for(int i=0;i<=s->top;i++)
+        {
+            printf("->%d",s->data[i]);
+        }
+        printf("\n");
+    }
+    
+}
+
 
 int main()
 {
     SeqStack s;
     Status flag;
+    ElemType e;
+    printf("%p\n",&s);
     flag = InitStatck(&s);
 
+    //入栈
+    flag = push(&s,3);
+    flag = push(&s,4);
+    flag = push(&s,5);
+    flag = push(&s,6);
+    flag = push(&s,7);
+    PrintStack(&s);
+    
+    //出栈
+    flag = pop(&s,&e);
+    printf("Stack(%p) pop=%d, current: ",&s,e);
+    PrintStack(&s);
 
+    flag = pop(&s,&e);
+    printf("Stack(%p) pop=%d, current: ",&s,e);
+    PrintStack(&s);
 
+    flag = pop(&s,&e);
+    printf("Stack(%p) pop=%d, current: ",&s,e);
+    PrintStack(&s);
 
+    flag = pop(&s,&e);
+    printf("Stack(%p) pop=%d, current: ",&s,e);
+    PrintStack(&s);
+
+    flag = pop(&s,&e);
+    printf("Stack(%p) pop=%d, current: ",&s,e);
+    PrintStack(&s);
 }
