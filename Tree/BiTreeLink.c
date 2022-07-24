cfe31fa78cb83073f0704d28d9f2729cfcb8c095
fazuo <ffzbest2008@gmail.com>
2022-07-24 23:47:04 +0800

    modified:   Tree/BiTreeLink.c

--- Tree/BiTreeLink.c
+++ Tree/BiTreeLink.c
@@ -8,7 +8,7 @@
 #include <math.h>
 #include <time.h>
 
-typedef int ElemType;
+typedef char ElemType;
 
 //定义二叉树结构
 typedef struct _BiTNode
@@ -17,8 +17,21 @@ typedef struct _BiTNode
     struct _BiTNode *lchild, *rchild; // 定义二叉树左右孩子
 } BiTNode, *BiTree;
 
+//创建二叉树
+void CreateBiTree(BiTree *T)
+{
+    ElemType ch;
+    scanf("%c", &ch);
+}
+
 int main()
 {
-    printf("OK");
+    printf("OK\n");
+
+    char x;
+    // x='A';
+    scanf("%c", &x);
+
+    printf("%c\n",x);
     return 0;
 }
