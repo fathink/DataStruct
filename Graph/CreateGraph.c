8344b9c04acd5060d8aef870c0f0d20b09d63b62
fathink <ffzbest2008@gmail.com>
2022-08-07 23:24:07 +0800

    modified:   Graph/CreateGraph.c     modified:   Tree/BiTreeLink.c

--- Graph/CreateGraph.c
+++ Graph/CreateGraph.c
@@ -3,7 +3,6 @@
 */
 
 
-
 typedef char VertexType;  //顶点类型
 typedef int EdgeType; //边类型
 #define MAXVEX 100
@@ -11,15 +10,36 @@ typedef int EdgeType; //边类型
 
 typedef struct _Graph
 {
-    VertexType vertexs[MAXVEX]; //顶点数组
+    VertexType vertexs[MAXVEX];  //顶点数组
     EdgeType arc[MAXVEX][MAXVEX];  //邻接矩阵
     int numNodes,numEdges; //当前顶点数和边数
     
 }MGraph;
 
 
+
+/*
+创建图
+          V0
+       /  |   \
+     V1   |    V3
+       \  |   /
+          V2
+*/
+void CreateGraph(MGraph *G)
+{
+     G->numNodes = 4; //顶点数
+    G->numEdges = 4; // 边数
+    
+   
+
+}
+
 int main()
 {
+    MGraph graph;
+    CreateGraph(&graph);
+
     
     return 0;
 }
