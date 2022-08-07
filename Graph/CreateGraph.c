aac8b73efd09dfbeebfb587a13229b08acd76040
fathink <ffzbest2008@gmail.com>
2022-08-08 00:17:12 +0800

    modified:   Graph/CreateGraph.c

--- Graph/CreateGraph.c
+++ Graph/CreateGraph.c
@@ -23,15 +23,15 @@ typedef struct _Graph
 创建图
           A
        /  |   \
-     B    |    D
+     B    E    D
        \  |   /
           C
 */
 void CreateGraph(MGraph *G)
 {
     int i,j;
-    G->numNodes = 4; //顶点数
-    G->numEdges = 4; // 边数
+    G->numNodes = 5; //顶点数
+    G->numEdges = 5; // 边数
 
     for(i=0;i<MAXVEX;i++)
         for(j=0;j<MAXVEX;j++)
@@ -42,13 +42,15 @@ void CreateGraph(MGraph *G)
     G->vertexs[1] = 'B';
     G->vertexs[2] = 'C';
     G->vertexs[3] = 'D';
+    G->vertexs[4] = 'E';
 
     //初始化边,无向图，对称矩阵
     G->arc[0][1] = 1; G->arc[1][0] = 1; 
-    G->arc[0][2] = 1; G->arc[2][0] = 1;
     G->arc[0][3] = 1; G->arc[3][0] = 1;
+    G->arc[0][4] = 1; G->arc[4][0] = 1;
     G->arc[1][2] = 1; G->arc[2][1] = 1;
     G->arc[2][3] = 1; G->arc[3][2] = 1;
+    G->arc[2][4] = 1; G->arc[4][2] = 1;
 
     //打印邻接矩阵
     printf("Adjacency Matrix:\n");
@@ -65,9 +67,18 @@ void CreateGraph(MGraph *G)
 /*
  深度优先遍历（DFS,Depth First Search） 
  */
-Bool visited[MAXVEX]; // 记录访问过的节点
+Bool visited[MAXVEX]={FALSE}; // 记录访问过的节点
 void DFS(MGraph G, int i)
 {
+    int j;
+    visited[i] = TRUE;
+    printf("%c",G.vertexs[i]); // 打印访问的节点
+
+    for(j=0;j<G.numNodes;j++)
+    {
+        if(G.arc[i][j] != INF && visited[j] != TRUE)
+            DFS(G,j);
+    }
 
 
 }
@@ -87,6 +98,9 @@ int main()
     MGraph graph;
     CreateGraph(&graph);
 
+    printf("\n深度优先遍历图:\n");
+    DFS(graph,0);
+    printf("\n");
     
     return 0;
 }
