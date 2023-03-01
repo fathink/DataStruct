a9f370ed75360c9d3fe4998799f152cc2eda6a84
fathink <ffzbest2008@gmail.com>
2023-03-01 21:57:22 +0800

    test/a.c 	test/b.c

--- test/hello.c
+++ test/hello.c
@@ -34,8 +34,8 @@ void sort(Edge *edge){
     int i,j;
     Edge e;
     
-    for(i=0;i<10;i++)
-        for(j=0;j<10-1;j++){
+    for(i=0;i<10-1;i++)
+        for(j=0;j<10-1-i;j++){
             if(edge[j].weight<edge[j+1].weight){
                 e = edge[j];
                 edge[j] = edge[j+1];
@@ -48,32 +48,31 @@ int main(){
     printf("\n");
     int i,j;
 
-    Edge edge[10];
-    for(i=0;i<10;i++){
-        edge[i].begin=i;
-        edge[i].end=i+1;
-        edge[i].weight=i*i;
-    }
-    for(i=0;i<10;i++){
-        printf("(%d,%d) %d\n",edge[i].begin,edge[i].end,edge[i].weight);
-    }
 
-    sort(edge);
-
-    printf("\nsorted:\n");
-    for(i=0;i<10;i++){
-        printf("(%d,%d) %d\n",edge[i].begin,edge[i].end,edge[i].weight);
-    }
+    int a[5]={1,2,3,4,5};
+    printf("%p,%p\n",a,a+1);
+    printf("%p\n",&a+1);
+    printf("==%d\n",*(a+1));
     
-
-
+    
     char c='w';
     char *p;
     p = &c;  //p指向c
 
-    typedef enum {A,B} POINT;
-    POINT po=B;
-    printf("%d\n",po);
+    char r='r';
+    char *z;
+    z = &r;  //z指向r
+    // *z='b';
+    // *z = *p;
+    
+    *z = *p;
+    printf("point_p=%p#%c,point_z=%p#%c\n",p,*p,z,*z);
+    printf("%p\n",&p);
+
+
+    // typedef enum {A,B} POINT;
+    // POINT po=B;
+    // printf("%d\n",po);
 
     return 0;
 }
