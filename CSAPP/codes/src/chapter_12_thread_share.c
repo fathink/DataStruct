7f26dfccf29dbfb43e2093f0aedda4a7b74afd7a
fathink <ffzbest2008@gmail.com>
2024-08-26 09:23:59 +0800

    renamed:    CSAPP/codes/src/csapp.c -> CSAPP/codes/include/csapp.c 	deleted:    CSAPP/codes/src/chapter_12.c 	new file:   CSAPP/codes/src/chapter_12_thread_share.c

---
+++ CSAPP/codes/src/chapter_12_thread_share.c
@@ -0,0 +1,32 @@
+/*
+ * 第十二章： 并发编程
+ *
+ * 
+ * 
+ * 
+*/
+
+#include "../include/csapp.h"
+
+#define N 2
+
+void * thread(void *vargp);
+
+char ** ptr; //全局指针
+
+int main(int args, char * argv[])
+{
+    int i;
+    pthread_t tid;
+
+    char *msgs[N] = {"hello from foo","hello from bar"};
+    
+    ptr = msgs;
+
+    return 0;
+}
+
+void * thread(void * vargp){
+
+    return NULL;
+}
