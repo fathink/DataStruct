1f23d8d2e15d61ff1c1cf28ba496140a4f0ff508
fathink <ffzbest2008@gmail.com>
2024-08-28 00:33:14 +0800

    modified:   ../CSAPP/codes/include/csapp.c 	modified:   ../CSAPP/codes/src/chapter_12_thread_share.c 	new file:   func_point.c

--- CSAPP/codes/src/chapter_12_thread_share.c
+++ CSAPP/codes/src/chapter_12_thread_share.c
@@ -1,33 +1,41 @@
 /*
- * 第十二章： 并发编程
- *
- * 
- * 
- * 
+ 第十二章： 并发编程
+ 
+ 测试多线程内存共享
+
 */
 
 #include "../include/csapp.h"
 
 #define N 2
 
-void * thread(void *vargp);
+void * thread_task(void *vargp);
 
-char ** ptr; //全局指针
+char ** PTR; //全局指针，不同线程均能通过该指针间接访问到主线程栈中的数据
 
 int main(int args, char * argv[])
 {
-    int i;
+    long i;
     pthread_t tid;
 
     char *msgs[N] = {"hello from foo","hello from bar"};
 
-    ptr = msgs;
-    printf("ok\n");
+    PTR = msgs;
+
+    for(i=0;i<N; i++)
+        Pthread_create(&tid, NULL, thread_task, (void *) i);
+
+    Pthread_exit(NULL);
 
     return 0;
 }
 
-void * thread(void * vargp){
+void * thread_task(void * vargp)
+{
+    long my_id = (long) vargp;
+    static int cnt = 0;  //cnt是全局变量，不同线程是共享的，A线程增加后会影响B线程
+    printf("thread_id=%d\n", pthread_self());
+    printf("task_id=[%ld]: %s, cnt=%d\n", my_id, PTR[my_id], ++cnt);
 
     return NULL;
 }
---
+++ c/func_point.c
@@ -0,0 +1,37 @@
+/*
+函数指针与指针函数
+*/
+
+#include <stdio.h>
+
+
+/*
+    函数指针，修饰指针，指针指向一个函数
+*/
+int (*p)(int x, int y);  //注意：这里的括号不能掉，因为括号()的运算优先级比解引用运算符*高
+
+
+/*
+    指针函数，指函数的返回值是一个指针
+*/
+int *p1(int x, int y);
+
+int max_value(int x, int y)
+{
+    return x>y ? x : y;
+}
+
+int main(int args, char * argv[])
+{
+    int (*p)(int, int) = NULL; //定义一个与maxValue兼容的指针
+    p = max_value;
+
+    int res = (*p)(2,3);  // 通过指针调用函数
+    // int res = p(2,3);  // 两种调用方式均可以
+    printf("res = %d\n", res);
+
+
+    printf("%p,%p",max_value, &max_value);
+
+    return 0;
+}
