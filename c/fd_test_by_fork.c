56bf89da3ef632beba35e9d755c0fdbb92981fa8
fathink <ffzbest2008@gmail.com>
2024-08-27 14:29:52 +0800

    modified:   fd_test_by_fork.c

--- c/fd_test_by_fork.c
+++ c/fd_test_by_fork.c
@@ -1,5 +1,5 @@
 /*
-* 采用fork函数对写文件的影响
+* 采用fork函数对读写文件描述符的影响
 
 
 - 文件表item结构体, 
@@ -7,13 +7,18 @@ struct file
     {
         mode_t f_mode;//表示文件是否可读或可写，FMODE_READ或FMODE_WRITE
         dev_ t  f_rdev ;// 用于/dev/tty
-        off_t  f_ops;//当前文件位移
+        off_t  f_ops;//当前文件偏移量
         unsigned short f_flags;//文件标志，O_RDONLY,O_NONBLOCK和O_SYNC
         unsigned short f_count;//打开的文件数目
         unsigned short f_reada;
         struct inode *f_inode;//指向inode的结构指针，实际存储内容的地方
         struct file_operations *f_op;//文件索引指针
     }
+
+
+Ref: 
+[1] 介绍Linux文件管理，含文件表数据结构等信息： https://blog.csdn.net/wwwlyj123321/article/details/100298377
+
 
 */
 
