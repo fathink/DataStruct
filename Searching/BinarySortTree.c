fdc7b7424d3bea054fc1103bc883ec22d42810b2
fathink <ffzbest2008@gmail.com>
2024-10-14 23:16:43 +0800

    new file:   BinarySortTree.c

---
+++ Searching/BinarySortTree.c
@@ -0,0 +1,21 @@
+/*
+    二叉排序树
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+
+typedef struct BiTNode
+{
+    int data; // 节点数据
+    struct BiTNode *lchild, *rchild; //左右节点
+}BiTNode, *BiTree;
+
+int main()
+{
+    printf("OK\n");
+}
